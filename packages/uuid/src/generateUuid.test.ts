bda77e0cdb6148cbab55fca238b7c43f5c5c01eb
Tylor Steinberger <tylor@tryfynd.com>
2017-10-29 19:45:55 -0400

    style: fix spelling error

--- packages/uuid/src/generateUuid.test.ts
+++ packages/uuid/src/generateUuid.test.ts
@@ -3,7 +3,7 @@ import { Test, describe, given, it } from '@typed/test'
 import { UuidArray } from './generateRandomNumbers'
 import { generateUuid } from './generateUuid'
 
-export const test: Test = describe(`generateUUid`, [
+export const test: Test = describe(`generateUuid`, [
   given(`[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]`, [
     it(`returns`, ({ equal }) => {
       const expected = '1234-56-478-89a-bcdef10'
