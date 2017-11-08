65a8384a39147e561b59851849345f675fb6de7c
Tylor Steinberger <tylor@tryfynd.com>
2017-11-08 12:32:57 -0500

    feat: Maybe.of === toMaybe
    Maybe.of now will convert null and undefined values to a Nothing.
    All other values will be wrapped in a Just. Additionally `toMaybe`
    has been removed from the public API in favor of `Maybe.of`.

--- packages/maybe/src/Maybe.ts
+++ packages/maybe/src/Maybe.ts
@@ -1,12 +1,14 @@
 import { Just } from './Just'
 import { Nothing } from './Nothing'
+import { toMaybe } from './toMaybe'
 
 export type Maybe<A> = Just<A> | Nothing
 
 export namespace Maybe {
   /**
-   * Creates a Maybe containg a value
+   * Creates a Maybe containing a value. If the value is `undefined` or `null`
+   * a `Nothing` will be returned. All other values will be wrapped in a `Just`.
    * @name Maybe.of<A>(value: A): Maybe<A>
    */
-  export const of: <A>(value: A) => Maybe<A> = Just.of
+  export const of: <A>(value: A) => Maybe<A> = toMaybe
 }
