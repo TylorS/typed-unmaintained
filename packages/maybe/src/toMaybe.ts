c26818e6d93eafdaf8ae78c68bd0d030e7bd81dd
Tylor Steinberger <tylor@tryfynd.com>
2017-11-08 15:50:25 -0500

    fix infinite loop

--- packages/maybe/src/toMaybe.ts
+++ packages/maybe/src/toMaybe.ts
@@ -1,6 +1,7 @@
+import { Just } from './Just'
 import { Maybe } from './Maybe'
 import { Nothing } from './Nothing'
 
 export function toMaybe<A>(value: A | void): Maybe<A> {
-  return value == null ? Nothing : Maybe.of<A>(value)
+  return value == null ? Nothing : Just.of<A>(value)
 }
