6efb81fc229eae4b2554be43d19c4c3addeac97f
Brian Cavalier <brian@briancavalier.com>
2018-08-25 21:24:28 -0400

    Fix curried chain type

--- packages/prelude/src/chain.ts
+++ packages/prelude/src/chain.ts
@@ -29,5 +29,5 @@ export type Chain = {
   <A, B>(f: (value: A, index: number) => List<B>): (list: List<A>) => Array<B>
   <A, B>(f: (value: A) => Maybe<B>): (maybe: Maybe<A>) => Maybe<B>
   <A, B>(f: (value: A) => PromiseLike<B>): (promise: PromiseLike<A>) => Promise<B>
-  <A, B, C>(f: (value: A) => Either<A, C>): (either: Either<A, B>) => Either<A, C>
+  <A, B, C>(f: (value: B) => Either<A, C>): (either: Either<A, B>) => Either<A, C>
 }
